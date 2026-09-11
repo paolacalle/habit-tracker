4da529ad0dac4fe7daf836fff9e2d04894600993
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-09-11 16:26:48 -0400

    define the post

--- habit-tracker/api/habits/route.ts
+++ habit-tracker/api/habits/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/internal-lib/prisma";
-import { arch } from "os";
 
 // define the GET method to fetch all habits from DB 
 export async function GET() {
@@ -15,7 +14,34 @@ export async function GET() {
         }
     );
 
-    return NextResponse.json(habits);
+    return NextResponse.json(habits, { status: 200 });
 }
 
 // define the POST method to create a new habit in DB
+export async function POST(request: Request) {
+
+    // parse the request body to get the habit data 
+    const body = await request.json();
+
+    // clean data 
+    const name = body.name.trim();
+    const description = body.description?.trim();
+
+    if (!name) {
+        return NextResponse.json(
+            { error: "Name is required" }, 
+            { status: 400 }
+        );
+    }
+
+    // create a new habit in the database using Prisma
+    const habit = await prisma.habit.create({
+        data: {
+            name: name,
+            description: description,
+            archived: false
+        }
+    });
+
+    return NextResponse.json(habit, { status: 201 });
+}
