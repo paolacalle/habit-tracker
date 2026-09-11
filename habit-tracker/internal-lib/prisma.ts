6dfc9f21f255ea25a2cab0d070d9a2c93eb41ffc
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-09-11 16:17:47 -0400

    push internal lib for resuable prisma conn

---
+++ habit-tracker/internal-lib/prisma.ts
@@ -0,0 +1,11 @@
+// Create one resuable connection accross the application 
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as { 
+    prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
